1a21f66834040cbf348e7ba53e942e3865473fe5
nareshtyse <48803828+nareshtyse@users.noreply.github.com>
2019-08-16 09:51:33 -0400

    Update resourcegroup.ps1

--- resourcegroup.ps1
+++ resourcegroup.ps1
@@ -1,1 +1,4 @@
- New-AzResourceGroup -Name apple  -Location EastUS
+Connect-AzAccount
+$AdminUsername=nareshbitla 
+$Adminpasswd = ConvertTo-SecureString Nareshbitla_1205 -AsPlainText -Force
+$pscredential = New-Object System.Management.Automation.PSCredential($AdminUsername, $Adminpasswd) New-AzResourceGroup -Name apple  -Location EastUS
